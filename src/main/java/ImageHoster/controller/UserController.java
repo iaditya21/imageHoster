ed820bc531a2b526557b47d2a022c86ea45bf415
iaditya21 <ishanaditya21@gmail.com>
2020-09-09 19:59:44 +0530

    password validation
    Signed-off-by: iaditya21 <ishanaditya21@gmail.com>

--- src/main/java/ImageHoster/controller/UserController.java
+++ src/main/java/ImageHoster/controller/UserController.java
@@ -10,9 +10,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.print.attribute.standard.PresentationDirection;
 import javax.servlet.http.HttpSession;
 import java.util.List;
+import java.util.regex.Pattern;
 
 
 @Controller
@@ -40,9 +43,17 @@ public class UserController {
     //This controller method is called when the request pattern is of type 'users/registration' and also the incoming request is of POST type
     //This method calls the business logic and after the user record is persisted in the database, directs to login page
     @RequestMapping(value = "users/registration", method = RequestMethod.POST)
-    public String registerUser(User user) {
-        userService.registerUser(user);
-        return "redirect:/users/login";
+    public String registerUser(User user, RedirectAttributes attributes) {
+        String regex="^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[@#$%^&-+=()]).{3,20}$";
+        Pattern pattern=Pattern.compile(regex);
+        if(pattern.matcher(user.getPassword()).matches()) {
+            userService.registerUser(user);
+            return "redirect:/users/login";
+        }
+        else {
+            attributes.addFlashAttribute("passwordTypeError","Password must contain at least 1 alphabet, 1 number & 1 special character");
+            return "redirect:/users/registration";
+        }
     }
 
     //This controller method is called when the request pattern is of type 'users/login'
